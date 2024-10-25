4478ee695fcc2814560e7b3016303d5ab9521241
CarloDettori <dettoric07@gmail.com>
2024-10-25 14:35:00 +0200

    done ex 2-3

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -8,7 +8,6 @@ console.log(name);
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
-
 let number = 8;
 let text = "dentifricio";
 let bool = true;
@@ -21,7 +20,7 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
 const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
 const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
 
-const fullName = null;
+const fullName = `${firstName} ${lastName};`
 console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
